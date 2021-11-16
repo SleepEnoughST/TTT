f4b60ca83eac9e820f1c3e0bf7780bc1655623c3
SleepEnoughST <108051085@live.asia.edu.tw>
2021-11-16 16:08:01 +0800

    2021/11/16

--- ttt/Assets/c#/player.cs
+++ ttt/Assets/c#/player.cs
@@ -7,21 +7,35 @@ public class player : MonoBehaviour
 
     public float moveSpeed;
     public Rigidbody2D rb;
-
+    public Animator animator;
+    
+    
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         Vector2 rubyMove = new Vector2();
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
         rubyMove = transform.position;
-        rubyMove.x = rubyMove.x + Input.GetAxis("Horizontal") * moveSpeed;
-        rubyMove.y = rubyMove.y + Input.GetAxis("Vertical") * moveSpeed;
-        //transform.position = rubyMove;
+        
+        animator.SetFloat("走路X", horizontal);
+        animator.SetFloat("走路Y", vertical);
+
+        rubyMove.x += moveSpeed * horizontal * Time.deltaTime;
+        rubyMove.y += moveSpeed * vertical * Time.deltaTime;
         rb.MovePosition(rubyMove);
+
+        if (horizontal == 0 && vertical == 0)
+        {
+            animator.SetTrigger("待機");
+        }
+        
     }
 }
